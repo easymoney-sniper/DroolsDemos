23defa7c9d5f69f4a01870d09fadfd7ed4487f2b
weixuan <weixuan@imairtime.cn>
2020-08-13 15:34:44 +0800

    Spring-boot集成Drools的Demo

---
+++ src/main/java/com/neo/drools/controller/TestController.java
@@ -0,0 +1,34 @@
+package com.neo.drools.controller;
+
+import com.neo.drools.model.Address;
+import com.neo.drools.model.fact.AddressCheckResult;
+import org.kie.api.runtime.KieSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+
+@RequestMapping("/test")
+@Controller
+public class TestController {
+
+    @Autowired
+    private KieSession kieSession;
+
+    @ResponseBody
+    @RequestMapping("/address")
+    public void test(){
+        Address address = new Address();
+        address.setPostcode("12345");
+
+        AddressCheckResult result = new AddressCheckResult();
+        kieSession.insert(address);
+        kieSession.insert(result);
+        int ruleFiredCount = kieSession.fireAllRules();
+        kieSession.destroy();
+        System.out.println("触发了" + ruleFiredCount + "条规则");
+    }
+}
